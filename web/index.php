5746915309e6da5f85ace6d21f9df6b0fde3b1d1
FaheerHakim <r0820158@student.thomasmore.be>
2024-01-10 21:26:30 +0100

    Added define composer.json in index.php

--- web/index.php
+++ web/index.php
@@ -9,4 +9,5 @@ require dirname(__DIR__) . '/bootstrap.php';
 // Load and run Craft
 /** @var craft\web\Application $app */
 $app = require CRAFT_VENDOR_PATH . '/craftcms/cms/bootstrap/web.php';
+define('CRAFT_COMPOSER_PATH', $root.'/composer.json');
 $app->run();
